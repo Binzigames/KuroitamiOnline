f025f595b8c60dfa4a971f8693867493e52d57f5
Artem Saveliev <113131407+minikumisoft@users.noreply.github.com>
2025-08-14 12:09:40 +0300

    Little clearer code and added credits(with little bug with clearing scene)

--- DATA/CORE/SceneManager.py
+++ DATA/CORE/SceneManager.py
@@ -1,15 +1,34 @@
 #------------->importing
 import DATA.Drawer as D
 import time as T
+from enum import Enum
+
+# Enums
+class ScenesEnum(Enum):
+    INTRO = 0
+    MENU = 1
+    CREDITS = 2
 
 #-------------> booling
-sceneInt = 0
+sceneInt = ScenesEnum.INTRO
+
+
 #------------->Scene manager logic
 def handle():
     global sceneInt
-    if sceneInt == 0:
+    if sceneInt == ScenesEnum.INTRO:
         D.intro_draw()
         T.sleep(5)
-        sceneInt += 1
-    elif sceneInt == 1:
+        sceneInt = ScenesEnum.MENU
+    elif sceneInt == ScenesEnum.MENU:
         D.menu_draw()
+        a = input()
+        if a == 'x':
+            exit()
+        elif a == 'c':
+            sceneInt = ScenesEnum.CREDITS
+    elif sceneInt == ScenesEnum.CREDITS:
+        D.credit_draw()
+        a = input()
+        if a == 'b':
+            sceneInt = ScenesEnum.MENU
--- DATA/Drawer.py
+++ DATA/Drawer.py
@@ -15,5 +15,15 @@ def menu_draw():
     sys.stdout.write(Fore.LIGHTYELLOW_EX + logo)
     sys.stdout.write(Fore.WHITE + "Devleoped by : Mizumi Studio \n")
 
-    sys.stdout.write(Fore.WHITE + "welcome to main menu...")
+    sys.stdout.write(Fore.WHITE + "welcome to main menu...\n")
+    sys.stdout.write(Fore.WHITE + "Choose option...\n")
+    sys.stdout.write(Fore.WHITE + "X - Exit\nC - Credits\n")
+
+def credit_draw():
+    logo = pyfiglet.figlet_format("Mizumi Studio", font="roman")
+    sys.stdout.write(Fore.RED + logo)
+
+    sys.stdout.write(Fore.WHITE + "Director, Lead Programmer - Porko\n")
+    sys.stdout.write(Fore.WHITE + "Programmer - Artem 'k_onna' Saveliev\n")
+    sys.stdout.write(Fore.WHITE + "Press B(ack) to main menu\n")
 
