3d3e7f16d35395f6804b08c4cfbe2f27ea97665f
Artem Saveliev <113131407+minikumisoft@users.noreply.github.com>
2025-08-14 16:13:14 +0300

    added loading screen

--- DATA/CORE/SceneManager.py
+++ DATA/CORE/SceneManager.py
@@ -10,14 +10,16 @@ class ScenesEnum(Enum):
     INTRO = 0
     MENU = 1
     CREDITS = 2
+    LOADING = 1488
 
 #-------------> booling
 sceneInt = ScenesEnum.INTRO
-
+toScreen = 0
 
 #------------->Scene manager logic
 def handle():
     global sceneInt
+    global toScreen
     if sceneInt == ScenesEnum.INTRO:
         D.intro_draw()
         T.sleep(5)
@@ -28,12 +30,18 @@ def handle():
         if a == 'x':
             exit()
         elif a == 'c':
-            sceneInt = ScenesEnum.CREDITS
+            sceneInt = ScenesEnum.LOADING
+            toScreen = ScenesEnum.CREDITS
     elif sceneInt == ScenesEnum.CREDITS:
         D.credit_draw()
         a = input()
         if a == 'b':
             sceneInt = ScenesEnum.MENU
+    
+    elif sceneInt == ScenesEnum.LOADING:
+        D.loading_scene()
+        T.sleep(5)
+        sceneInt = toScreen
     if platform.system() == "Windows":
         os.system("cls")
     else:
--- DATA/Drawer.py
+++ DATA/Drawer.py
@@ -1,6 +1,7 @@
 #-------------> import
 import pyfiglet
 import sys
+import random
 from colorama import Fore
 
 #-------------> levels functions draw
@@ -20,10 +21,18 @@ def menu_draw():
     sys.stdout.write(Fore.WHITE + "X - Exit\nC - Credits\n")
 
 def credit_draw():
-    logo = pyfiglet.figlet_format("Mizumi Studio", font="roman")
+    logo = pyfiglet.figlet_format("Mizumi Studio", font="colossal")
     sys.stdout.write(Fore.RED + logo)
 
     sys.stdout.write(Fore.WHITE + "Director, Lead Programmer - Porko\n")
     sys.stdout.write(Fore.WHITE + "Programmer - Artem 'k_onna' Saveliev\n")
     sys.stdout.write(Fore.WHITE + "Press B(ack) to main menu\n")
+
+def loading_scene():
+    logo = pyfiglet.figlet_format("Loading...", font="colossal")
+    ebubu = ["Dinahu1\n", "Dinahu2\n", "Dinahu3\n"]
+    sys.stdout.write(Fore.BLUE + logo)
+    sys.stdout.write(Fore.WHITE + random.choice(ebubu))
+
+
 
