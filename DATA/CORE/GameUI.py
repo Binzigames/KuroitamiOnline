2d039c0e75cc17ab52f889eed395b5af0f2d413e
porko <yehor.kovalia@gmail.com>
2025-08-25 15:46:42 +0300

    Update GameUI.py

--- DATA/CORE/GameUI.py
+++ DATA/CORE/GameUI.py
@@ -5,8 +5,11 @@ import sys
 import time
 from colorama import Fore
 import DATA.CORE.storage as s
+import DATA.CORE.ArtsUI as art
+import DATA.CORE.Character as C
 #-------------> draw functions
-
+avatar= C.Character
+a = input()
 def main_screen():
     if s.FirsTime == True:
         sys.stdout.write(Fore.WHITE + f"welcome to the zone , {Fore.RED + s.Pname}\n")
@@ -14,8 +17,75 @@ def main_screen():
         sys.stdout.write(Fore.WHITE + "Welcome to the Exclusion Zone - a post-apocalyptic world where the catastrophe of 2000 has changed reality.\n Explore abandoned cities, forests and rifts, survive among mutants, fight for rare materials and uncover the secret of the Soul of the Zone.\n Create a character, join a faction and become a legend of the Zone. Only you decide which path to choose.\n Dare to challenge the Zone.\n")
         sys.stdout.write(Fore.RED + "================\n")
         sys.stdout.write(Fore.WHITE + "enter 'C' to continue")
-        if input() == "c" and s.FirsTime:
-            s.FirsTime = False
+        if a == "c" and s.FirsTime:
+            char_create()
+
+def char_create():
+    # > draw
+    sys.stdout.write(Fore.YELLOW + art.peoples)
+    sys.stdout.write(Fore.RED + "================\n")
+    sys.stdout.write(Fore.WHITE + "CREATE YOUR CHARACTER...\n")
+    sys.stdout.write(Fore.YELLOW + "OPTIONS :\n")
+    sys.stdout.write(Fore.RED + "================\n")
+    sys.stdout.write(Fore.RED + f"strength - {avatar.strength}, agility - {avatar.agility}, intelligence - {avatar.intelligence}, charisma - {avatar.charisma}, endurance - {avatar.endurance}, luck - {avatar.luck}")
+    sys.stdout.write(Fore.RED + "================\n")
+    sys.stdout.write(Fore.WHITE + "enter 'add <option>' to in-cris\n")
+    sys.stdout.write(Fore.WHITE + "enter 'min <option>' to de-cris\n")
+
+    # >func
+    points = 20
+    #incrce
+    if a == "add ":
+        opt = float(a.split(" ", 1)[1])
+        #check in opt(inos) in input
+        if opt == "strength" and not avatar.strength > 10 :
+            avatar.strength += 1
+            points -= 1
+        elif opt == "agility" and not avatar.agility > 10 :
+            avatar.agility += 1
+            points -= 1
+        elif opt == "intelligence" and not avatar.intelligence > 10 :
+            avatar.intelligence += 1
+            points -= 1
+        elif opt == "charisma" and not avatar.charisma > 10 :
+            avatar.charisma += 1
+            points -= 1
+        elif opt == "endurance" and not avatar.endurance > 10 :
+            avatar.endurance += 1
+            points -= 1
+        elif opt == "luck" and not avatar.luck > 10 :
+            avatar.luck += 1
+            points -= 1
+    #decrce
+    if a == "min ":
+        opt = float(a.split(" ", 1)[1])
+        #check in opt(inos) in input
+        if opt == "strength" and not avatar.strength > 10 :
+            avatar.strength -= 1
+            points += 1
+        elif opt == "agility" and not avatar.agility > 10 :
+            avatar.agility -= 1
+            points += 1
+        elif opt == "intelligence" and not avatar.intelligence > 10 :
+            avatar.intelligence -= 1
+            points += 1
+        elif opt == "charisma" and not avatar.charisma > 10 :
+            avatar.charisma -= 1
+            points += 1
+        elif opt == "endurance" and not avatar.endurance > 10 :
+            avatar.endurance -= 1
+            points += 1
+        elif opt == "luck" and not avatar.luck > 10 :
+            avatar.luck -= 1
+            points += 1
+
+
+
+
+
+
+
+
 
 
 def reconect_screen():
